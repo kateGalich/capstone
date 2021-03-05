cf2e852f129832b794c54be7efd75b0524943be5
kateGalich <kate.galich@outlook.com>
2021-03-05 14:41:11 -0500

    Move classes to their own files

--- BetterBooks/BetterBooks/Models/ApplicationDbContext.cs
+++ BetterBooks/BetterBooks/Models/ApplicationDbContext.cs
@@ -1,48 +1,9 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Data.Entity;
-using System.Security.Claims;
-using System.Threading.Tasks;
+﻿using System.Data.Entity;
 using BetterBooks.Migrations;
-using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace BetterBooks.Models
 {
-    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser
-    {
-        [StringLength(50)]
-        public string Phone { get; set; }
-
-        [StringLength(150)]
-        public string HomeAddress { get; set; }
-
-        //public virtual ICollection<BookReview> BookReviews { get; set; }
-        //public virtual ICollection<BorrowedBook> BorrowedBooks { get; set; }
-        public virtual ICollection<Book> Books { get; set; }
-        public virtual ICollection<Book> RequestedBooks { get; set; }
-        //public virtual ICollection<Book> GivingAwayBooks { get; set; }
-
-        public ApplicationUser()
-        {
-            //BookReviews = new HashSet<BookReview>();
-            //BorrowedBooks = new HashSet<BorrowedBook>();
-            Books = new HashSet<Book>();
-            RequestedBooks = new HashSet<Book>();
-            //BooksOfferedToMe = new HashSet<Book>();
-            //GivingAwayBooks = new HashSet<Book>();
-        }
-
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
-        {
-            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
-            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
-            // Add custom user claims here
-            return userIdentity;
-        }
-    }
-
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
