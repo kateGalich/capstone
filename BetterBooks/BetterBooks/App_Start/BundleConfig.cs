acf9e11fe158f8dc9570479224034126cab7690e
Janvi39 <janvi.garg57@gmail.com>
2021-04-16 11:49:49 -0400

    Changes done in pages

--- BetterBooks/BetterBooks/App_Start/BundleConfig.cs
+++ BetterBooks/BetterBooks/App_Start/BundleConfig.cs
@@ -19,8 +19,8 @@ namespace BetterBooks
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap")
-                .Include("~/Scripts/bootstrap.bundle.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
